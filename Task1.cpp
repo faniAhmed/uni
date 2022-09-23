230ed56829e59e7785810c8417302a43897493bf
faniAhmed <89826402+faniAhmed@users.noreply.github.com>
2022-09-23 18:10:24 +0500

    Update and rename Untitled4.cpp to Task1.cpp

---
+++ Task1.cpp
@@ -0,0 +1,15 @@
+/*
+Write a program and take number from user and print its cube.
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	
+	cout<<"Enter Any Number:";
+	cin>>n;
+
+	cout<<"Cube = " << n*n*n;
+	return 0;
+}

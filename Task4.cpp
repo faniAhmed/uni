ff2a4c84b79b61579445a066db4ab4c4ada5d13c
faniAhmed <89826402+faniAhmed@users.noreply.github.com>
2022-09-24 18:06:36 +0500

    Update Task4.cpp

--- Task4.cpp
+++ Task4.cpp
@@ -4,19 +4,12 @@ Write a C++ program to find mean of ten numbers read from users.
 
 #include<iostream>
 using namespace std;
-
 int main(){
-	int n, sum = 0, Avg, arr[10];
-	
-	cout<<"Enter Ten Intergers to find Average:";
-	for(n=0;n<10;n++)
-	    cin>>arr[n];
-	
-	for(n=0;n<10;n++)
-	    sum = sum + arr[n];
-		
-	Avg = sum/10;
-	
+	int n, num, sum = 0, Avg;
+	cout<<"Enter Ten Integers to find Average:";	
+	for(n=0;n<10;n++){
+	    cin>>num;
+	    sum = sum + num; }
+	Avg = sum/10;	
 	cout<<"Average = " << Avg;
-	return 0;
-}
+	return 0;  }
